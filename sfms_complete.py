16940cf09e2da97a69a5c839282afc92466d9b48
Shakil <121shakilahmed@gmail.com>
2025-05-21 13:01:23 +0600

    backend classes added

--- sfms_complete.py
+++ sfms_complete.py
@@ -3,3 +3,16 @@ from tkinter import ttk, messagebox
 import datetime
 import uuid
 import json
+
+# Backend Classes
+class Member:
+    def __init__(self, name, age, membership_type, fitness_goals):
+        self.member_id = str(uuid.uuid4())[:8]
+        self.name = name
+        self.age = age
+        self.membership_type = membership_type
+        self.fitness_goals = fitness_goals
+        self.progress_data = []
+        self.workouts = []
+        self.meals = []
+        self.goals = {}
